12290a09fa78de09bc32bd3faa6720c6326ad74e
alzyoodlara89 <lara.ma7mood2012@gmail.com>
2021-03-02 16:06:05 +0200

    ready

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let numofmeal=2;
+let age=30;
+const dis=0.10;
+console.log("before changes:,numofmeal,age,dis");
+age=13;
+console.log("after changes: numofmeal,age,dis");
+
+alert('Hello everyone');
+
+user=prompt('how many meal you are want?');
+userout=prompt("is there any prpblem in service");
+userface=prompt("enter your name");
+userInput=prompt('enter your age: ');
+
+ 
+alert ("you get discount");
